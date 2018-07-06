f2c1102b855a094350fff375d8a351388c625b23
Tomoki Kyotani <tkyotani121@gmail.com>
2018-07-06 12:01:42 -0700

    Removed the redandancies and unnecessary events happening

--- CharacterCount/js/CharCount.js
+++ CharacterCount/js/CharCount.js
@@ -14,39 +14,35 @@ jQuery.noConflict();
     var textFieldLabel = config.textFieldLabel;
     var countFieldLabel = config.countFieldLabel;
     var regex = /[^_\W]+/g;
-    var outerTextFieldArray, outerCountFieldArray,
-        setTextFieldIndex, setCountFieldIndex,
-        textFieldArray, countFieldArray,
+    var textFieldArray, countFieldArray,
+        setTextField, setCountField,
         setTextFieldClassName, setCountFieldClassName;
 
     kintone.events.on(['app.record.create.show', 'app.record.edit.show'], function(event) {
-        //These elements include the label name
-        outerTextFieldArray = Array.from(document.getElementsByClassName("control-multiple_line_text-field-gaia"));
-        //Using the config.label, find the index of the user-set-textField
-        //This index indicates which field is set by an user.
-        outerTextFieldArray.forEach(function(element, index){
+        //These elements include the label names
+        textFieldArray = Array.from(document.getElementsByClassName("control-multiple_line_text-field-gaia"));
+        //Using the config.label, get the element of the user-set-textField
+        textFieldArray.forEach(function(element, index){
             if(textFieldLabel === element.textContent){
-                setTextFieldIndex = index;
+                setTextField = textFieldArray[index];
             }
-        })
-        outerCountFieldArray = Array.from(document.getElementsByClassName("control-decimal-field-gaia"));
-        outerCountFieldArray.forEach(function(element, index){
+        });
+
+        countFieldArray = Array.from(document.getElementsByClassName("control-decimal-field-gaia"));
+        countFieldArray.forEach(function(element, index){
             if(countFieldLabel === element.textContent){
-                setCountFieldIndex = index;
+                setCountField = countFieldArray[index];
             }
-        })
+        });
 
-        //These elements store the values like texts/numbers
-        textFieldArray = Array.from(document.getElementsByClassName("textarea-cybozu"));
-        countFieldArray = Array.from(document.getElementsByClassName("input-text-cybozu input-number-cybozu"));
-        countFieldArray[setCountFieldIndex].disabled = true;
+        setCountField.children[1].children[0].children[1].children[0].children[0].disabled = true;
 
-        textFieldArray[setTextFieldIndex].onkeyup = function(e){
+        setTextField.onkeyup = function(e){
             var text = e.target.value;
             var match = text.match(regex);
             var length = match.join('').length;
 
-            countFieldArray[setCountFieldIndex].value = length;
+            setCountField.children[1].children[0].children[1].children[0].children[0].value = length;
         };
     })
 
@@ -58,30 +54,18 @@ jQuery.noConflict();
     })
 
     kintone.events.on('app.record.index.edit.show', function(event){
-        //These arrays contain just the elements of user-set fields being editted.
-        textFieldArray = Array.from(document.getElementsByClassName("recordlist-editcell-gaia recordlist-edit-multiple_line_text-gaia " + setTextFieldClassName));
-        countFieldArray = Array.from(document.getElementsByClassName("recordlist-editcell-gaia recordlist-edit-decimal-gaia " + setCountFieldClassName));
+        //Get the element of the user-set-textField
+        setTextField = document.getElementsByClassName("recordlist-editcell-gaia recordlist-edit-multiple_line_text-gaia " + setTextFieldClassName)[0];
+        setCountField = document.getElementsByClassName("recordlist-editcell-gaia recordlist-edit-decimal-gaia " + setCountFieldClassName)[0];
 
-        countFieldArray[0].children[0].children[0].children[1].children[0].disabled = true;
+        setCountField.children[0].children[0].children[1].children[0].disabled = true;
 
-        textFieldArray[0].onkeyup = function(e){
-            console.log("hey");
+        setTextField.onkeyup = function(e){
             var text = e.target.value;
             var match = text.match(regex);
             var length = match.join('').length;
 
-            countFieldArray[0].children[0].children[0].children[1].children[0].value = length
+            setCountField.children[0].children[0].children[1].children[0].value = length;
         };
-
-        //This recognizes just the user-set text field too since "setTextFieldClassName" is unique to that field
-            //and keyup is triggered only on the field that is being editted
-        // $("." + setTextFieldClassName).keyup(function(e){
-        //     //This property holds the value
-        //     var text = textFieldArray[0].children[0].children[0].value;
-        //     var match = text.match(regex);
-        //     var length = match.join('').length;
-        //
-        //     countFieldArray[0].children[0].children[0].children[1].children[0].value = length;
-        // });
     })
 })(jQuery, kintone.$PLUGIN_ID);
